a45d88ccadeb650047dfdc85e632ea4c8d47be09
Ahmed Sakr <asakr1191@gmail.com>
2022-11-18 17:04:50 +0200

    Add files via upload

--- algorithms.py
+++ algorithms.py
@@ -1,74 +1,54 @@
 import sys
+from treelib import Tree, Node
 
 
-def minimax(state, k):
-    beststate = recursion(state, (2 * k - 1), 1)
-
-
-def recursion(state, k, flag, alpha, beta):
-    if k == 1:
-        max = -sys.maxsize
-        children = getchildren(state)
-        for child in children:
-            h = heuristic(child)
-            if h > max:
-                max = h
-
-        return [child, max]
-
-    if flag == 2:
-        children = getchildren(state)
-        max = -sys.maxsize
-        for child in children:
-            r = recursion(child, k - 1, 0)
-            value = r[1]
-            if value > max:
-                max = value
-
-    elif flag == 1:
-        children = getchildren(state)
-        Min = sys.maxsize
-        for child in children:
-            value = recursion(child, k - 1, 1)
-            if value < Min:
-                Min = value
-
-
-def heuristic(state):
+def heuristic(state: list[list[str]]) -> int:
     score = getScore(state)
-    h1 = score[0] - score[1]
+    heu = 10 * (score[0] - score[1])
+    # heu += 4 * check_children(state, '1')
+    # heu += 2 * check_children(state, '2')
+    heu -= 3 * checkDefiniteTwo(state)
+    heu += 4 * checkDefiniteFour(state, '2')
+    heu -= 4 * checkDefiniteFour(state, '1')
+    heu += checkDefiniteThreeInRow(state, '2')
+    heu -= checkDefiniteThreeInRow(state, '1')
+    heu += checkDefiniteThreeInDiagonal(state, '2')
+    heu -= checkDefiniteThreeInDiagonal(state, '1')
+    heu += checkThree(state, '2')
+    heu -= checkThree(state, '1')
+    heu += checkTwo(state, '2')
+    heu -= checkTwo(state, '1')
+    return heu
 
 
-def is_full(state):
+def is_full(state: list[list[str]]) -> bool:
     for col in range(7):
         for row in range(6):
-            if state[row][col] == 0:
-                return 0
-    return 1
+            if state[row][col] == '0':
+                return False
+    return True
 
 
-def getchildren(state, flag):
+def getchildren(state: list[list[str]], flag: str) -> list[list[list[str]]]:
     children = []
     for col in range(7):
         for row in range(6):
             x = state[row][col]
-            if x == 0:
+            if x == '0':
                 child = [state[i].copy() for i in range(6)]
-                if flag == 1:
+                if flag == '1':
                     child[row][col] = '1'
                     children.append(child)
                 else:
                     child[row][col] = '2'
                     children.append(child)
                 break
-
     return children
 
 
 def getScore(state: list[list[str]]) -> tuple:
     agent = 0
     user = 0
-
     # check rows
     for i in range(6):
         for j in range(4):
@@ -87,7 +67,7 @@ def getScore(state: list[list[str]]) -> tuple:
                     user += 1
                 if state[i][j] == '2':
                     agent += 1
-    # check diagonals
+    # check diagonals from the left
     for i in range(3):
         for j in range(4):
             if state[i][j] != '0' and state[i][j] == state[i + 1][j + 1] and state[i][j] == state[i + 2][j + 2] and \
@@ -96,6 +76,7 @@ def getScore(state: list[list[str]]) -> tuple:
                     user += 1
                 if state[i][j] == '2':
                     agent += 1
+    # check diagonals from the right
     for i in range(3):
         for j in range(6, 2, -1):
             if state[i][j] != '0' and state[i][j] == state[i + 1][j - 1] and state[i][j] == state[i + 2][j - 2] and \
@@ -104,11 +85,7 @@ def getScore(state: list[list[str]]) -> tuple:
                     user += 1
                 if state[i][j] == '2':
                     agent += 1
-
     return agent, user
-
-def get_search_tree(map):  # {state, [h, p]}):0'''
-    pass
 
 
 def check_children(state, flag):
@@ -118,12 +95,10 @@ def check_children(state, flag):
 
     for child in children:
         newAgent, newUser = getScore(child)
-        if flag == 1:
+        if flag == '1':
             result -= newUser - user
-
         else:
             result += newAgent - agent
-
     return result
 
 
@@ -143,28 +118,338 @@ def check_zeros(state, flag):
                     newState[i][j] = '2'
                     newAgent = getScore(newState)[0]
                     result += newAgent - agent
+    return result
 
+
+def checkDefiniteTwo(state: list[list[str]]) -> int:
+    result = 0
+    for i in range(6):
+        for j in range(3):
+            # 0,0,1,1,0
+            if state[i][j] == '0' and state[i][j + 1] == '0' and state[i][j + 2] == '1' and state[i][j + 3] == '1' and \
+                    state[i][j + 4] == '0':
+                if i == 0:
+                    result += 1
+                else:
+                    if state[i - 1][j] != '0' and state[i - 1][j + 1] != '0' and state[i - 1][j + 4] != '0':
+                        result += 1
+            # 0,1,1,0,0
+            if state[i][j] == '0' and state[i][j + 1] == '1' and state[i][j + 2] == '1' and state[i][j + 3] == '0' and \
+                    state[i][j + 4] == '0':
+                if i == 0:
+                    result += 1
+                else:
+                    if state[i - 1][j] != '0' and state[i - 1][j + 3] != '0' and state[i - 1][j + 4] != '0':
+                        result += 1
+    return result
+
+
+def checkDefiniteThreeInRow(state: list[list[str]], flag: str) -> int:
+    result = 0
+    for i in range(6):
+        for j in range(3):
+            # 0,1,1,1,0
+            if state[i][j] == '0' and state[i][j + 1] == flag and state[i][j + 2] == flag and \
+                    state[i][j + 3] == flag and state[i][j + 4] == '0':
+                if i == 0:
+                    result += 1
+                else:
+                    if state[i - 1][j] != '0' and state[i - 1][j + 4] != '0':
+                        result += 1
+    return result
+
+
+def checkDefiniteThreeInDiagonal(state: list[list[str]], flag: str) -> int:
+    result = 0
+    #               0
+    #           1
+    #       1
+    #   1
+    # 0
+    for i in range(2):
+        for j in range(3):
+            if state[i][j] == '0' and state[i + 4][j + 4] == '0' and state[i + 1][j + 1] == flag and state[i + 2][
+                j + 2] == flag and state[i + 3][j + 3] == flag:
+                if i == 0 and state[i + 3][j + 4] != '0':
+                    result += 1
+                else:
+                    if state[i - 1][j] != '0' and state[i + 3][j + 4] != '0':
+                        result += 1
+    # 0
+    #   1
+    #       1
+    #           1
+    #               0
+    for i in range(2):
+        for j in range(6, 3, -1):
+            if state[i][j] == '0' and state[i + 4][j - 4] == '0' and state[i + 1][j - 1] == flag and state[i + 2][
+                j - 2] == flag and state[i + 3][j - 3] == flag:
+                if i == 0 and state[i + 3][j - 4] != '0':
+                    result += 1
+                else:
+                    if state[i - 1][j] != '0' and state[i + 3][j - 4] != '0':
+                        result += 1
+    return result
+
+
+def checkDefiniteFour(state: list[list[str]], flag: str) -> int:
+    result = 0
+    for i in range(1, 6):
+        for j in range(4):
+            # 0,1,1,1
+            # 0,1,1,1
+            if state[i][j] == '0' and state[i][j + 1] == flag and state[i][j + 2] == flag and state[i][j + 3] == flag:
+                if state[i - 1][j] == '0' and state[i - 1][j + 1] == flag and state[i - 1][j + 2] == flag and \
+                        state[i - 1][j + 3] == flag:
+                    result += 1
+            # 1,1,1,0
+            # 1,1,1,0
+            if state[i][j] == flag and state[i][j + 1] == flag and state[i][j + 2] == flag and state[i][j + 3] == '0':
+                if state[i - 1][j] == flag and state[i - 1][j + 1] == flag and state[i - 1][j + 2] == flag and \
+                        state[i - 1][j + 3] == '0':
+                    result += 1
+            # 1,0,1,1
+            # 1,0,1,1
+            if state[i][j] == flag and state[i][j + 1] == '0' and state[i][j + 2] == flag and state[i][j + 3] == flag:
+                if state[i - 1][j] == flag and state[i - 1][j + 1] == '0' and state[i - 1][j + 2] == flag and \
+                        state[i - 1][j + 3] == flag:
+                    result += 1
+            # 1,1,0,1
+            # 1,1,0,1
+            if state[i][j] == flag and state[i][j + 1] == flag and state[i][j + 2] == '0' and state[i][j + 3] == flag:
+                if state[i - 1][j] == flag and state[i - 1][j + 1] == flag and state[i - 1][j + 2] == '0' and \
+                        state[i - 1][j + 3] == flag:
+                    result += 1
     return result
 
 
 def checkThree(state: list[list[str]], flag: str) -> int:
+    result = 0
+    # check rows
     for i in range(6):
-        for j in range(3):
-            if state[i][j] == '0' and state[i][j + 1] == flag and state[i][j + 2] == flag and\
-                    state[i][j + 3] == flag and state[i][j + 4] == '0':
-                if i == 0:
-                    return 1
-                else:
-                    if state[i - 1][j] != '0' and state[i - 1][j + 4] != '0':
-                        return 1
-    return 0
+        for j in range(4):
+            # 0,1,1,1
+            if state[i][j] == '0' and state[i][j + 1] == flag and state[i][j + 2] == flag and state[i][j + 3] == flag:
+                result += 1
+            # 1,1,1,0
+            if state[i][j] == flag and state[i][j + 1] == flag and state[i][j + 2] == flag and state[i][j + 3] == '0':
+                result += 1
+    # check columns
+    for j in range(7):
+        for i in range(3):
+            # 0
+            # 1
+            # 1
+            # 1
+            if state[i][j] == flag and state[i + 1][j] == flag and state[i + 2][j] == flag and state[i + 3][j] == '0':
+                result += 1
+    # check diagonals from the left
+    for i in range(3):
+        for j in range(4):
+            if state[i][j] == flag and state[i + 1][j + 1] == flag and state[i + 2][j + 2] == flag and state[i + 3][
+                j + 3] == '0':
+                result += 1
+            if state[i][j] == '0' and state[i + 1][j + 1] == flag and state[i + 2][j + 2] == flag and state[i + 3][
+                j + 3] == flag:
+                result += 1
+    # check diagonals from the right
+    for i in range(3):
+        for j in range(6, 2, -1):
+            if state[i][j] == flag and state[i + 1][j - 1] == flag and state[i + 2][j - 2] == flag and state[i + 3][
+                j - 3] == '0':
+                result += 1
+            if state[i][j] == '0' and state[i + 1][j - 1] == flag and state[i + 2][j - 2] == flag and state[i + 3][
+                j - 3] == flag:
+                result += 1
+    return result
 
 
-if __name__ == '__main__':
-    flag = 2
-    state = [[1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1],
-             [1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1]]
-    # print(state)
-    # getchildren(state, flag)
-    x = is_full(state)
-    print(x)
+def checkTwo(state: list[list[str]], flag: str) -> int:
+    result = 0
+    # check rows
+    for i in range(6):
+        for j in range(4):
+            # 0,0,1,1
+            if state[i][j] == '0' and state[i][j + 1] == '0' and state[i][j + 2] == flag and state[i][j + 3] == flag:
+                result += 1
+            # 1,1,0,0
+            if state[i][j] == flag and state[i][j + 1] == flag and state[i][j + 2] == '0' and state[i][j + 3] == '0':
+                result += 1
+            # 0,1,1,0
+            if state[i][j] == '0' and state[i][j + 3] == '0' and state[i][j + 1] == flag and state[i][j + 2] == flag:
+                result += 1
+    # check columns
+    for j in range(7):
+        for i in range(3):
+            # 0
+            # 1
+            # 1
+            if state[i][j] == flag and state[i + 1][j] == flag and state[i + 2][j] == '0':
+                result += 1
+    # check diagonals from the left
+    for i in range(3):
+        for j in range(4):
+            #      0
+            #    0
+            #  1
+            # 1
+            if state[i][j] == flag and state[i + 1][j + 1] == flag and state[i + 2][j + 2] == '0' and state[i + 3][
+                j + 3] == '0':
+                result += 1
+            #      0
+            #    1
+            #  1
+            # 0
+            if state[i][j] == '0' and state[i + 3][j + 3] == '0' and state[i + 1][j + 1] == flag and state[i + 2][
+                j + 2] == flag:
+                result += 1
+            #       1
+            #    1
+            #  0
+            # 0
+            if state[i][j] == '0' and state[i + 1][j + 1] == '0' and state[i + 2][j + 2] == flag and state[i + 3][
+                j + 3] == flag:
+                result += 1
+    # check diagonals from the right
+    for i in range(3):
+        for j in range(6, 2, -1):
+            # 0
+            #  0
+            #    1
+            #      1
+            if state[i][j] == flag and state[i + 1][j - 1] == flag and state[i + 2][j - 2] == '0' and state[i + 3][
+                j - 3] == '0':
+                result += 1
+            # 0
+            #  1
+            #    1
+            #      0
+            if state[i][j] == '0' and state[i + 3][j - 3] == '0' and state[i + 1][j - 1] == flag and state[i + 2][
+                j - 2] == flag:
+                result += 1
+            # 1
+            #   1
+            #     0
+            #       0
+            if state[i][j] == '0' and state[i + 1][j - 1] == '0' and state[i + 2][j - 2] == flag and state[i + 3][
+                j - 3] == flag:
+                result += 1
+    return result
+
+
+def minimax(state: list[list[str]], k: int, pruning: bool, showTree: bool):
+    alpha = -sys.maxsize
+    beta = sys.maxsize
+    tree = Tree()
+    identifier = 1
+
+    def minmax(state: list[list[str]], k: int, flag: str, parent):
+        nonlocal tree
+        nonlocal identifier
+        c = 0
+        if k == 1 and flag == '2':
+            maximum = -sys.maxsize
+            children = getchildren(state, '2')
+            for child in children:
+                h = heuristic(child)
+                tree.create_node(str(h), parent=parent.identifier)
+                if h > maximum:
+                    maximum = h
+                    c = child
+                    parent.tag = "Max " + str(maximum)
+            return c, maximum
+
+        if k == 1 and flag == '1':
+            minimum = sys.maxsize
+            children = getchildren(state, '1')
+            for child in children:
+                h = heuristic(state)
+                tree.create_node(str(h), parent=parent.identifier)
+                if h < minimum:
+                    minimum = h
+                    c = child
+                    parent.tag = "Min " + str(minimum)
+            return c, minimum
+
+        if flag == '2':
+            children = getchildren(state, '2')
+            maximum = -sys.maxsize
+            for child in children:
+                parent_id = parent.identifier
+                parent = tree.create_node("Max", identifier, parent)
+                identifier += 1
+                value = minmax(child, k - 1, '1', parent)
+                maximum = max(maximum, value[1])
+                parent = tree.get_node(parent_id)
+                if maximum == value[1]:
+                    c = child
+                    parent.tag = "Max " + str(maximum)
+
+            return c, maximum
+
+        if flag == '1':
+            children = getchildren(state, '1')
+            minimum = sys.maxsize
+            for child in children:
+                parent_id = parent.identifier
+                parent = tree.create_node("Min", identifier, parent)
+                identifier += 1
+                value = minmax(child, k - 1, '2', parent)
+                minimum = min(minimum, value[1])
+                parent = tree.get_node(parent_id)
+                if minimum == value[1]:
+                    c = child
+                    parent.tag = "Min " + str(minimum)
+
+            return c, minimum
+
+    def minmaxPruning(state: list[list[str]], k: int, flag: str):
+        nonlocal alpha
+        nonlocal beta
+        c = 0
+        if k == 1:
+            maximum = -sys.maxsize
+            children = getchildren(state, '2')
+            for child in children:
+                h = heuristic(child)
+                if h > maximum:
+                    maximum = h
+                    c = child
+            return c, maximum
+
+        if flag == '2':
+            children = getchildren(state, '2')
+            maximum = -sys.maxsize
+            for child in children:
+                value = minmaxPruning(child, k - 1, '1')
+                maximum = max(maximum, value[1])
+                alpha = max(alpha, value[1])
+                if maximum == value[1]:
+                    c = child
+
+                if beta <= alpha:
+                    break
+
+            return c, maximum
+
+        if flag == '1':
+            children = getchildren(state, '1')
+            minimum = sys.maxsize
+            for child in children:
+                value = minmaxPruning(child, k - 1, '2')
+                minimum = min(minimum, value[1])
+                beta = min(beta, value[1])
+
+                if minimum == value[1]:
+                    c = child
+
+                if beta <= alpha:
+                    break
+
+            return c, minimum
+
+    result = minmaxPruning(state, k, '2') if pruning else minmax(state, k, '2', tree.create_node("Max", 0))
+    if showTree:
+        tree.show()
+
+    return result
--- backend.py
+++ backend.py
@@ -1,4 +1,6 @@
 import random
+import time
+
 from PyQt5.QtCore import QTimer
 from frontend import Ui_MainWindow
 from PyQt5 import QtWidgets
@@ -43,64 +45,81 @@ class CONNECT4(QtWidgets.QMainWindow):
         self.cols = [self.col0, self.col1, self.col2, self.col3, self.col4, self.col5, self.col6]
 
         self.timer.timeout.connect(self.blink)
-        self.ui.button0.clicked.connect(lambda: self.set_state(0, str(self.turn), self.col0))
-        self.ui.button1.clicked.connect(lambda: self.set_state(1, str(self.turn), self.col1))
-        self.ui.button2.clicked.connect(lambda: self.set_state(2, str(self.turn), self.col2))
-        self.ui.button3.clicked.connect(lambda: self.set_state(3, str(self.turn), self.col3))
-        self.ui.button4.clicked.connect(lambda: self.set_state(4, str(self.turn), self.col4))
-        self.ui.button5.clicked.connect(lambda: self.set_state(5, str(self.turn), self.col5))
-        self.ui.button6.clicked.connect(lambda: self.set_state(6, str(self.turn), self.col6))
-        self.ui.button7.clicked.connect(lambda: self.set_state(0, str(self.turn), self.col0))
-        self.ui.button8.clicked.connect(lambda: self.set_state(1, str(self.turn), self.col1))
-        self.ui.button9.clicked.connect(lambda: self.set_state(2, str(self.turn), self.col2))
-        self.ui.button10.clicked.connect(lambda: self.set_state(3, str(self.turn), self.col3))
-        self.ui.button11.clicked.connect(lambda: self.set_state(4, str(self.turn), self.col4))
-        self.ui.button12.clicked.connect(lambda: self.set_state(5, str(self.turn), self.col5))
-        self.ui.button13.clicked.connect(lambda: self.set_state(6, str(self.turn), self.col6))
-        self.ui.button14.clicked.connect(lambda: self.set_state(0, str(self.turn), self.col0))
-        self.ui.button15.clicked.connect(lambda: self.set_state(1, str(self.turn), self.col1))
-        self.ui.button16.clicked.connect(lambda: self.set_state(2, str(self.turn), self.col2))
-        self.ui.button17.clicked.connect(lambda: self.set_state(3, str(self.turn), self.col3))
-        self.ui.button18.clicked.connect(lambda: self.set_state(4, str(self.turn), self.col4))
-        self.ui.button19.clicked.connect(lambda: self.set_state(5, str(self.turn), self.col5))
-        self.ui.button20.clicked.connect(lambda: self.set_state(6, str(self.turn), self.col6))
-        self.ui.button21.clicked.connect(lambda: self.set_state(0, str(self.turn), self.col0))
-        self.ui.button22.clicked.connect(lambda: self.set_state(1, str(self.turn), self.col1))
-        self.ui.button23.clicked.connect(lambda: self.set_state(2, str(self.turn), self.col2))
-        self.ui.button24.clicked.connect(lambda: self.set_state(3, str(self.turn), self.col3))
-        self.ui.button25.clicked.connect(lambda: self.set_state(4, str(self.turn), self.col4))
-        self.ui.button26.clicked.connect(lambda: self.set_state(5, str(self.turn), self.col5))
-        self.ui.button27.clicked.connect(lambda: self.set_state(6, str(self.turn), self.col6))
-        self.ui.button28.clicked.connect(lambda: self.set_state(0, str(self.turn), self.col0))
-        self.ui.button29.clicked.connect(lambda: self.set_state(1, str(self.turn), self.col1))
-        self.ui.button30.clicked.connect(lambda: self.set_state(2, str(self.turn), self.col2))
-        self.ui.button31.clicked.connect(lambda: self.set_state(3, str(self.turn), self.col3))
-        self.ui.button32.clicked.connect(lambda: self.set_state(4, str(self.turn), self.col4))
-        self.ui.button33.clicked.connect(lambda: self.set_state(5, str(self.turn), self.col5))
-        self.ui.button34.clicked.connect(lambda: self.set_state(6, str(self.turn), self.col6))
-        self.ui.button35.clicked.connect(lambda: self.set_state(0, str(self.turn), self.col0))
-        self.ui.button36.clicked.connect(lambda: self.set_state(1, str(self.turn), self.col1))
-        self.ui.button37.clicked.connect(lambda: self.set_state(2, str(self.turn), self.col2))
-        self.ui.button38.clicked.connect(lambda: self.set_state(3, str(self.turn), self.col3))
-        self.ui.button39.clicked.connect(lambda: self.set_state(4, str(self.turn), self.col4))
-        self.ui.button40.clicked.connect(lambda: self.set_state(5, str(self.turn), self.col5))
-        self.ui.button41.clicked.connect(lambda: self.set_state(6, str(self.turn), self.col6))
+        self.ui.button0.clicked.connect(lambda: self.set_state(0, str(self.turn)))
+        self.ui.button1.clicked.connect(lambda: self.set_state(1, str(self.turn)))
+        self.ui.button2.clicked.connect(lambda: self.set_state(2, str(self.turn)))
+        self.ui.button3.clicked.connect(lambda: self.set_state(3, str(self.turn)))
+        self.ui.button4.clicked.connect(lambda: self.set_state(4, str(self.turn)))
+        self.ui.button5.clicked.connect(lambda: self.set_state(5, str(self.turn)))
+        self.ui.button6.clicked.connect(lambda: self.set_state(6, str(self.turn)))
+        self.ui.button7.clicked.connect(lambda: self.set_state(0, str(self.turn)))
+        self.ui.button8.clicked.connect(lambda: self.set_state(1, str(self.turn)))
+        self.ui.button9.clicked.connect(lambda: self.set_state(2, str(self.turn)))
+        self.ui.button10.clicked.connect(lambda: self.set_state(3, str(self.turn)))
+        self.ui.button11.clicked.connect(lambda: self.set_state(4, str(self.turn)))
+        self.ui.button12.clicked.connect(lambda: self.set_state(5, str(self.turn)))
+        self.ui.button13.clicked.connect(lambda: self.set_state(6, str(self.turn)))
+        self.ui.button14.clicked.connect(lambda: self.set_state(0, str(self.turn)))
+        self.ui.button15.clicked.connect(lambda: self.set_state(1, str(self.turn)))
+        self.ui.button16.clicked.connect(lambda: self.set_state(2, str(self.turn)))
+        self.ui.button17.clicked.connect(lambda: self.set_state(3, str(self.turn)))
+        self.ui.button18.clicked.connect(lambda: self.set_state(4, str(self.turn)))
+        self.ui.button19.clicked.connect(lambda: self.set_state(5, str(self.turn)))
+        self.ui.button20.clicked.connect(lambda: self.set_state(6, str(self.turn)))
+        self.ui.button21.clicked.connect(lambda: self.set_state(0, str(self.turn)))
+        self.ui.button22.clicked.connect(lambda: self.set_state(1, str(self.turn)))
+        self.ui.button23.clicked.connect(lambda: self.set_state(2, str(self.turn)))
+        self.ui.button24.clicked.connect(lambda: self.set_state(3, str(self.turn)))
+        self.ui.button25.clicked.connect(lambda: self.set_state(4, str(self.turn)))
+        self.ui.button26.clicked.connect(lambda: self.set_state(5, str(self.turn)))
+        self.ui.button27.clicked.connect(lambda: self.set_state(6, str(self.turn)))
+        self.ui.button28.clicked.connect(lambda: self.set_state(0, str(self.turn)))
+        self.ui.button29.clicked.connect(lambda: self.set_state(1, str(self.turn)))
+        self.ui.button30.clicked.connect(lambda: self.set_state(2, str(self.turn)))
+        self.ui.button31.clicked.connect(lambda: self.set_state(3, str(self.turn)))
+        self.ui.button32.clicked.connect(lambda: self.set_state(4, str(self.turn)))
+        self.ui.button33.clicked.connect(lambda: self.set_state(5, str(self.turn)))
+        self.ui.button34.clicked.connect(lambda: self.set_state(6, str(self.turn)))
+        self.ui.button35.clicked.connect(lambda: self.set_state(0, str(self.turn)))
+        self.ui.button36.clicked.connect(lambda: self.set_state(1, str(self.turn)))
+        self.ui.button37.clicked.connect(lambda: self.set_state(2, str(self.turn)))
+        self.ui.button38.clicked.connect(lambda: self.set_state(3, str(self.turn)))
+        self.ui.button39.clicked.connect(lambda: self.set_state(4, str(self.turn)))
+        self.ui.button40.clicked.connect(lambda: self.set_state(5, str(self.turn)))
+        self.ui.button41.clicked.connect(lambda: self.set_state(6, str(self.turn)))
 
-    def set_state(self, j, char, col):
+    def set_state(self, j, char):
         if char == '1':
             self.ui.frame.setEnabled(False)
             for i in range(6):
-                if i == 5:
-                    for button in col:
-                        button.setEnabled(False)
-
                 if self.state[i][j] == '0':
                     self.state[i][j] = char
                     self.display_state()
                     self.turn = 2
                     self.ui.turn.setStyleSheet("background-color: rgb(255, 255, 0); border-radius: 50px")
+                    k = self.ui.maxDepth.value()
+                    start = time.time()
+                    self.display_state()
+                    self.disable()
+                    self.state = minimax(self.state, k, self.ui.pruning.isChecked(), self.ui.searchTree.isChecked())[0]
+                    self.ui.turn.setStyleSheet("background-color: rgb(255, 0, 0); border-radius: 50px")
+                    end = time.time()
+                    self.turn = 1
+                    self.display_state()
+                    self.disable()
+                    self.ui.frame.setEnabled(True)
+                    print(end - start)
                     return
 
+    def disable(self):
+        for j, col in enumerate(self.cols):
+            if self.state[-1][j] != '0':
+                for button in col:
+                    button.setEnabled(False)
+
+        if is_full(self.state):
+            self.timer.stop()
+            self.ui.turn.hide()
 
     def display_state(self):
         for j, col in enumerate([self.col0, self.col1, self.col2, self.col3, self.col4, self.col5, self.col6]):
@@ -126,7 +145,7 @@ class CONNECT4(QtWidgets.QMainWindow):
     def reset(self):
         self.state = [['0' for _ in range(7)] for _ in range(6)]
         self.turn = random.randrange(1, 3, 1)  # 1 for user's turn, 2 for computer turn
-        if self.turn:
+        if self.turn == 1:
             self.ui.turn.setStyleSheet("background-color: rgb(255, 0, 0); border-radius: 50px")
         else:
             self.ui.turn.setStyleSheet("background-color: rgb(255, 255, 0); border-radius: 50px")
